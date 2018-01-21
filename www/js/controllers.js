f911399864b4ccc2ff3185d481caa70e95435082
HARRISON\harrison <harrison.croaker@hotmail.com>
2018-01-21 14:12:20 +1000

    Page 1,2 and 3 pretty much complete

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,25 @@
 angular.module('starter.controllers', [])
 
 .controller('HomePageCtrl', function($scope, $state) {
+  $scope.options = {
+    loop: false,
+    speed: 500,
+  }
+
+  $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
+    // data.slider is the instance of Swiper
+    $scope.slider = data.slider;
+  });
+
+  $scope.$on("$ionicSlides.slideChangeStart", function(event, data){
+    console.log('Slide change is beginning');
+  });
+
+  $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
+    // note: the indexes are 0-based
+    $scope.activeIndex = data.slider.activeIndex;
+    $scope.previousIndex = data.slider.previousIndex;
+  });
 
   $scope.goToInstructionsPage = function() {
       $state.go('instructionsPage');
@@ -25,7 +44,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('LbwPageCtrl', function($scope, $state) {
-  
+
   $scope.goToDecisionPage = function() {
       $state.go('decisionPage');
   };
