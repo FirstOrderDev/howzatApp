d1969d20c99b53b113fedd4152369f8ea0462c16
HARRISON\harrison <harrison.croaker@hotmail.com>
2018-01-21 14:35:12 +1000

    Fonts and video

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -37,6 +37,18 @@ angular.module('starter.controllers', [])
 
 .controller('SetUpPageCtrl', function($scope, $state) {
 
+  // Grab elements, create settings, etc.
+  var video = document.getElementById('video');
+
+  // Get access to the camera!
+  if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Not adding `{ audio: true }` since we only want video now
+      navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+          video.src = window.URL.createObjectURL(stream);
+          video.play();
+      });
+  }
+  
   $scope.goToLbwPage = function() {
       $state.go('lbwPage');
   };
